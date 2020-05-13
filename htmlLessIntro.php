76786cc10d4b2645938626bcad845d75a5803bc4
unknown <dvandenbrink@roc-dev.com>
2020-05-13 16:16:34 +0200

    frond end patch

--- htmlLessIntro.php
+++ htmlLessIntro.php
@@ -11,14 +11,17 @@
     <header>
         <img src="bitacademy logo.png" alt="">
     </header>
-    <div>
-    <main>
+    <div class="main">
         <h1>HTML</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam qui totam fugit tempore alias voluptate veritatis deserunt corrupti eveniet consequatur, debitis dignissimos non aliquid quia ipsa nemo saepe officia!</p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam qui totam fugit tempore alias voluptate veritatis deserunt corrupti eveniet consequatur, debitis dignissimos non aliquid quia ipsa nemo saepe officia!</p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam qui totam fugit tempore alias voluptate veritatis deserunt corrupti eveniet consequatur, debitis dignissimos non aliquid quia ipsa nemo saepe officia!</p>
-    </main>
-    <button>Begin >></button> 
-    </div>   
+    </div>
+    
+    <div class="buttonFlex">
+        <button><< Home page</button>
+        <button>Begin >></button> 
+    </div>
+
 </body>
-</html> 
+</html>
