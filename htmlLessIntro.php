90a9f83bfcefe25109f93beb43e50382237bae1d
unknown <dvandenbrink@roc-dev.com>
2020-05-13 15:12:13 +0200

    html intro pagina

---
+++ htmlLessIntro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="htmlLessIntro.css">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <img src="bitacademy logo.png" alt="">
+    </header>
+    <div>
+    <main>
+        <h1>HTML</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam qui totam fugit tempore alias voluptate veritatis deserunt corrupti eveniet consequatur, debitis dignissimos non aliquid quia ipsa nemo saepe officia!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam qui totam fugit tempore alias voluptate veritatis deserunt corrupti eveniet consequatur, debitis dignissimos non aliquid quia ipsa nemo saepe officia!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam qui totam fugit tempore alias voluptate veritatis deserunt corrupti eveniet consequatur, debitis dignissimos non aliquid quia ipsa nemo saepe officia!</p>
+    </main>
+    <button>Begin >></button> 
+    </div>   
+</body>
+</html> 
