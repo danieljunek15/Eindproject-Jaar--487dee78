db88b62f5f9777ccc60b2bd631ce0a3c417e29fc
unknown <dvandenbrink@roc-dev.com>
2020-05-13 14:08:30 +0200

    home pagina V0.1

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Bitorial.com</title>
+    <link rel="stylesheet" href="style.index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div class="head">
+        <h1>Bitorial</h1>
+    </div>
+    <button class="Setup">Setup</button>
+    <button class="html">HTML</button>
+    <div class="wrapper">
+        <button class="css">CSS</button>
+        <button class="git">GIT</button>
+    </div>
+    <button class="php">PHP</button>
+
+
+</body>
+
+</html>
