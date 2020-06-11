1f651f2b8cf5f75f5ab1e84bcee4316527c6cec8
Tom Paulus <tom.paulus@student.rocvf.nl>
2020-06-11 11:33:43 +0200

    Home page nieuw

--- script.index.js
+++ script.index.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-    $("#myModal").modal('show');
-});
-
 var c = document.getElementById("lineone");
 var ctx = c.getContext("2d");
 ctx.beginPath();
@@ -9,19 +5,7 @@ ctx.moveTo(0, 0);
 ctx.lineTo(0, 150);
 ctx.stroke();
 
-var c = document.getElementById("linetwo");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(500, 300);
-ctx.stroke();
 
-var c = document.getElementById("linethree");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(0, 300);
-ctx.lineTo(500, 0);
-ctx.stroke();
 
 var c = document.getElementById("linefour");
 var ctx = c.getContext("2d");
